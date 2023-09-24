cc07fde3648a8ec5a20ae2e97b32be606cd04791
srijan <srijan.dhl21@gmail.com>
2023-09-24 20:00:52 +1000

    small bug fixed

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -77,17 +77,19 @@ export const Navbar = () => {
             </div>
           </Link>
           <Link
-            to={"/cart"}
+            to={"/fav"}
             className="py-4 px-1 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out"
           >
             <span className="sr-only">Fav</span>
             <div className="flex">
               <img src={fav} alt="fav" />
-              <span class="absolute inset-0 object-right-top -mr-6">
-                <div class="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
-                  {user.favouriteProduct?.length}
-                </div>
-              </span>
+              {user.favouriteProduct?.length && (
+                <span class="absolute inset-0 object-right-top -mr-6">
+                  <div class="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
+                    {user.favouriteProduct?.length}
+                  </div>
+                </span>
+              )}
             </div>
           </Link>
 
